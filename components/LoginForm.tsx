aee2e6582861df77e566ddc48f6c916d6a08a304
herol3oy <hamedsedighi@gmail.com>
2025-06-01 18:41:38 +0200

    remove comment

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -39,7 +39,6 @@ export function LoginForm({
         password,
       })
       if (error) throw error
-      // Update this route to redirect to an authenticated route. The user already has an active session.
       router.push('/dashboard')
     } catch (error: unknown) {
       setError(error instanceof Error ? error.message : 'An error occurred')
