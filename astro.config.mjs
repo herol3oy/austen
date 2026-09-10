7600475f4be5d540f0ad4fe1ea8c20c0a98f7549
Hamed Sedighi <hamedsedighi@gmail.com>
2026-09-10 21:00:34 +0200

    add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({ site: 'https://herol3oy.github.io', base: '/austen/', output: 'static', trailingSlash: 'always' });
+export default defineConfig({
+  site: 'https://herol3oy.github.io',
+  base: '/austen/',
+  output: 'static',
+  trailingSlash: 'always',
+  integrations: [sitemap()]
+});
