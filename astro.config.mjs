c987f0bdd460e6705e27b4bda77070a2fc5df6fa
Hamed Sedighi <hamedsedighi@gmail.com>
2026-09-12 16:24:57 +0200

    update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://herol3oy.github.io',
-  base: '/austen/',
+  site: 'https://austen.page',
   output: 'static',
   trailingSlash: 'always',
   integrations: [sitemap()]
